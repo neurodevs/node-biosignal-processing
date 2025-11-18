ec25a16d17dde2ae0ac99ca24949075d36269a7b
ericthecurious <hello@ericthecurious.com>
2025-11-17 17:01:18 -0800

    patch: fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-//exports go here
+// JitterGrapher
+
+export { default as TimestampJitterGrapher } from './impl/TimestampJitterGrapher.js'
+export * from './impl/TimestampJitterGrapher.js'
+
+export { default as FakeJitterGrapher } from './testDoubles/JitterGrapher/FakeJitterGrapher.js'
+export * from './testDoubles/JitterGrapher/FakeJitterGrapher.js'
