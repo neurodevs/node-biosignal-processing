26398d4463aa6da08e2b8876ba92607f91672f81
ericthecurious <hello@ericthecurious.com>
2025-11-18 23:52:49 -0800

    patch: generates jitter plot

--- src/__tests__/impl/TimestampJitterGrapher.test.ts
+++ src/__tests__/impl/TimestampJitterGrapher.test.ts
@@ -2,11 +2,14 @@ import { randomInt } from 'crypto'
 import { callsToWriteFile } from '@neurodevs/fake-node-core'
 import generateId from '@neurodevs/generate-id'
 import { test, assert } from '@neurodevs/node-tdd'
-
 import { FakeXdfLoader, XdfStream } from '@neurodevs/node-xdf'
+import { Data } from 'plotly.js-dist-min'
+
 import TimestampJitterGrapher, {
     JitterGrapher,
 } from '../../impl/TimestampJitterGrapher.js'
+import FakePlotly from '../../testDoubles/Plotly/FakePlotly.js'
+import { resetCallsToPlotlyToImage } from '../../testDoubles/Plotly/FakePlotly.js'
 import AbstractPackageTest from '../AbstractPackageTest.js'
 
 export default class TimestampJitterGrapherTest extends AbstractPackageTest {
@@ -19,6 +22,8 @@ export default class TimestampJitterGrapherTest extends AbstractPackageTest {
         this.setFakeWriteFile()
 
         FakeXdfLoader.fakeResponse = this.fakeXdfFile
+
+        this.setFakeToImage()
 
         this.instance = await this.TimestampJitterGrapher()
     }
@@ -61,6 +66,77 @@ export default class TimestampJitterGrapherTest extends AbstractPackageTest {
             },
             'Did not write results to JSON file!'
         )
+    }
+
+    @test()
+    protected static async generatesJitterPlot() {
+        await this.run()
+
+        assert.isEqualDeep(
+            callsToWriteFile[1],
+            {
+                file: `${this.outputDir}/jitter.png`,
+                data: await this.generatePngBuffer(),
+                options: undefined,
+            },
+            'Did not generate jitter plot PNG file!'
+        )
+    }
+
+    private static async generatePngBuffer() {
+        const traces = this.fakeStreams.map((stream) => {
+            return {
+                x: [],
+                y: [],
+                type: 'histogram',
+                histnorm: 'density',
+                name: stream.name,
+                autobinx: false,
+                opacity: 0.5,
+                marker: {
+                    color: 'rgba(255, 100, 102, 0.7)',
+                    line: {
+                        color: 'rgba(255, 100, 102, 1)',
+                        width: 1,
+                    },
+                },
+                xbins: {
+                    end: 2.8,
+                    size: 0.06,
+                    start: 0.5,
+                },
+            } as Data
+        })
+
+        let layout = {
+            bargap: 0.05,
+            bargroupgap: 0.2,
+            barmode: 'overlay' as const,
+            title: {
+                text: 'Distribution of Timestamp Jitter per Stream',
+            },
+            xaxis: {
+                title: {
+                    text: 'Timestamp Jitter (ms)',
+                },
+            },
+            yaxis: {
+                title: {
+                    text: 'Count',
+                },
+            },
+        }
+
+        const pngBuffer = await FakePlotly.toImage(
+            { data: traces, layout },
+            {
+                format: 'png',
+                width: 1000,
+                height: 600,
+            }
+        )
+
+        return pngBuffer
     }
 
     private static async run() {
@@ -113,6 +189,11 @@ export default class TimestampJitterGrapherTest extends AbstractPackageTest {
         return this.fakeStreams.map(
             ({ data: _data, timestamps: _timestamps, ...rest }) => rest
         )
+    }
+
+    private static setFakeToImage() {
+        TimestampJitterGrapher.toImage = FakePlotly.toImage.bind(this)
+        resetCallsToPlotlyToImage()
     }
 
     private static async TimestampJitterGrapher() {
